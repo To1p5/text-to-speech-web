98c62beb722668c961b8d49b1f2946a26a31d6e3
John <fmsalli@hotmail.com>
2025-01-19 23:46:24 -0800

    new comments

--- Am.py
+++ Am.py
@@ -3,7 +3,7 @@ import PyPDF2
 from pydub import AudioSegment
 import os
 
-#Bob Bob Bob
+#Bob Bob Bob jom
 def create_audio(file_path, output_name):
     # Initialize speaker
     speaker = pyttsx3.init()
